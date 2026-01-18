5ed6ed59c9314d42f8ea16f4be1c26f08382cce5
adityapandey78 <adityapandey.2402@gmail.com>
2026-01-18 19:50:02 +0530

    added shivesh credentials

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -94,7 +94,7 @@ function Footer() {
                                 <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
                             </svg>
                         </span>
-                        by Aditya Pandey
+                        by Shivesh Kumar
                     </p>
                     
                     {/* Social Media Links */}
@@ -120,7 +120,7 @@ function Footer() {
                             </svg>
                         </a>
                         <a 
-                            href="https://www.linkedin.com/in/aditya-pandey-1a96b0213/" 
+                            href="https://www.linkedin.com/in/shiveshkumar56" 
                             target="_blank" 
                             rel="noopener noreferrer"
                             className="text-gray-600 hover:text-violet-600 dark:text-gray-300 dark:hover:text-cyan-400 transition-colors"
@@ -130,7 +130,7 @@ function Footer() {
                                 <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
                             </svg>
                         </a>
-                        <a 
+                        {/* <a 
                             href="https://x.com/adityapandey78" 
                             target="_blank" 
                             rel="noopener noreferrer"
@@ -140,7 +140,7 @@ function Footer() {
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                                 <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                             </svg>
-                        </a>
+                        </a> */}
                     </div>
                 </div>
             </div>
